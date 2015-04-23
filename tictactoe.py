bf8ed0e7bf84966a012822db292eff7a6cdb655c
James Stroehmann <jstroehmann@comcast.net>
2015-04-22 21:39:00 -0400

    testing git push ssh key

--- tictactoe.py
+++ tictactoe.py
@@ -3,7 +3,7 @@
 import random
 
 def drawBoard(board):
-	print('   |   |')
+	print('   |   | ')
 	print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
 	print('   |   |')
 	print('-----------')
